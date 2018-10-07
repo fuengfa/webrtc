5a07974f97ec6afb275b02e7dfe0fa79e3a50fc7
bique14 <peerasorn14@gmail.com>
2018-10-07 09:10:33 +0700

    fix typo and style

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, View, TouchableOpacity } from 'react-native';
+import { Text, View, TouchableOpacity, StyleSheet } from 'react-native';
 import { Camera, Permissions } from 'expo';
 
 export default class App extends React.Component {
@@ -13,6 +13,14 @@ export default class App extends React.Component {
     this.setState({ hasCameraPermission: status === 'granted' });
   }
 
+  switchScreen() {
+    this.setState({
+      type: this.state.type === Camera.Constants.Type.back
+        ? Camera.Constants.Type.front
+        : Camera.Constants.Type.back,
+    })
+  }
+
   render() {
     const { hasCameraPermission } = this.state;
     if (hasCameraPermission === null) {
@@ -21,30 +29,14 @@ export default class App extends React.Component {
       return <Text>No access to camera</Text>;
     } else {
       return (
-        <View style={{ flex: 1 }}>
-          <Camera style={{ flex: 1, aspectRatio: 3/4 }} type={this.state.type}>
-            <View
-              style={{
-                flex: 1,
-                backgroundColor: 'transparent',
-                flexDirection: 'row',
-              }}>
-              <TouchableOpacity
-                style={{
-                  flex: 0.1,
-                  alignSelf: 'flex-end',
-                  alignItems: 'center',
-                }}
-                onPress={() => {
-                  this.setState({
-                    type: this.state.type === Camera.Constants.Type.back
-                      ? Camera.Constants.Type.front
-                      : Camera.Constants.Type.back,
-                  });
-                }}>
-                <Text
-                  style={{ fontSize: 18, marginBottom: 10, color: 'white' }}>
-                  {' '}Flip{' '}
+        <View style={styles.container}>
+          <Camera style={styles.camera} type={this.state.type}>
+            <View style={styles.cameraView}>
+              <TouchableOpacity style={styles.touchable}
+                onPress={this.switchScreen.bind(this)}
+              >
+                <Text style={styles.switchScreen}>
+                  Switch Screen
                 </Text>
               </TouchableOpacity>
             </View>
@@ -54,3 +46,28 @@ export default class App extends React.Component {
     }
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  camera: {
+    flex: 1, 
+    aspectRatio: 3/4
+  },
+  cameraView: {
+    flex: 1,
+    backgroundColor: 'transparent',
+    flexDirection: 'row',
+  },
+  touchable: {
+    flex: 0.1,
+    alignSelf: 'flex-end',
+    alignItems: 'center',
+  },
+  switchScreen: {
+    fontSize: 14, 
+    marginBottom: 10, 
+    color: 'white'
+  }
+})
